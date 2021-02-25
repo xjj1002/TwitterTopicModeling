a4a1d1328116d2812c90edb7ac3ecc8e8209bebf
xjj1002 <xjj1002@sru.edu>
2021-02-25 01:06:46 -0500

    User create login and request endpoints

--- TwitterTopicModeling/Controllers/UsersController.cs
+++ TwitterTopicModeling/Controllers/UsersController.cs
@@ -2,51 +2,80 @@ namespace TwitterTopicModeling.Controllers
 {
 
 
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Threading.Tasks;
-  using Microsoft.AspNetCore.Mvc;
-  using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
 
-  using Flurl;
-  using Flurl.Http;
+    using Flurl;
+    using Flurl.Http;
 
-  using System.Threading;
-  using System.Collections;
-  using TwitterTopicModeling.Database.Models;
-  using TwitterTopicModeling.Database;
-  using Microsoft.EntityFrameworkCore;
+    using System.Threading;
+    using System.Collections;
+    using Microsoft.EntityFrameworkCore;
 
-  [ApiController]
-  [Route("[controller]")]
-  public class UsersController : ControllerBase
-  {
-    public ILogger<TwitterUsersController> Logger { get; }
+    //my classes
+    using TwitterTopicModeling.Database.Models;
+    using TwitterTopicModeling.Database;
+    using TwitterTopicModeling.Payloads;
 
-    public TwitterContext TwitterContext;
-
-
-
-    public UsersController(ILogger<TwitterUsersController> logger, TwitterContext twitterContext)
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
     {
+        public ILogger<TwitterUsersController> Logger { get; }
 
-      Logger = logger;
-      TwitterContext = twitterContext;
+        public TwitterContext TwitterContext;
+
+        public UsersController(ILogger<TwitterUsersController> logger, TwitterContext twitterContext)
+        {
+
+            Logger = logger;
+            TwitterContext = twitterContext;
 
 
+        }
+
+        [HttpGet("getUser/{username}")]
+        public async Task<User> getUser(string UserName)
+        {
+
+            var DatabaseUser = await TwitterContext.Users
+           .Where(x => x.userName == UserName)
+           .FirstOrDefaultAsync();
+
+            return DatabaseUser;
+
+        }
+
+        [HttpPost("createUser")]
+        public async Task<User> createUser(UserDTO user)
+        {
+
+            var insertedUser = await TwitterContext.Users
+                .AddAsync(new User
+                {
+                    userName = user.username,
+                    password = user.password
+                });
+
+            await TwitterContext.SaveChangesAsync();
+            return insertedUser.Entity;
+        }
+
+
+        [HttpPost("login")]
+        public async Task<bool> login(UserDTO user)
+        {
+
+            return await TwitterContext.Users
+                .AnyAsync(x => 
+                    x.userName == user.username && 
+                    x.password == user.password
+                );
+
+        }
     }
-
-    [HttpGet]
-    public async Task<User> getUser(string UserName)
-    {
-
-      var DatabaseUser = await TwitterContext.Users
-     .Where(x => x.userName == UserName)
-     .FirstOrDefaultAsync();
-
-      return DatabaseUser;
-
-    }
-  }
 }
---
+++ TwitterTopicModeling/Payloads/UserDTO.cs
@@ -0,0 +1,9 @@
+namespace TwitterTopicModeling.Payloads
+{
+    public class UserDTO
+    {
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
