875ad38bcab6c7b4551ee19e1ef684338398bf3f
xjj1002 <xjj1002@sru.edu>
2021-04-12 15:13:37 -0400

    flagForReport

---
+++ TwitterTopicModeling/Migrations/20210412151801_RepoprtFlad.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TwitterTopicModeling.Migrations
+{
+    public partial class RepoprtFlad : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "malFlag",
+                table: "Report",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "malFlag",
+                table: "Report");
+        }
+    }
+}
---
+++ TwitterTopicModeling/Migrations/20210412153105_RepoprtFladDTO.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TwitterTopicModeling.Migrations
+{
+    public partial class RepoprtFladDTO : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
